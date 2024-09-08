5113105519a71900646b679e3f2026c026b84809
Felix Plamper <felix.plamper@tuta.io>
2024-09-08 15:41:32 +0200

    Rekey for nas

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,10 +2,12 @@ let
   pc-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKqykgN7RuOz+6YCDWYTeXfGKRHT5VXG/LJWGN1zFro";
   test-vm-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMVaNdLGdOsuczVoGG9jhtRI02kt+PgCdo5Mfhcn+OwN";
   test-vm-key-admin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwN68PStruvwNlb91jPuUlhLQyCgKP/umNGo1Bo+d5G";
-  keys = [ pc-key test-vm-key test-vm-key-admin ];
+  nas-admin-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPwAlKNggifROXV1CBjjVNwgkSSa3t15muTo40ZeysS";
+  nas-host-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJQddURjXQPlTzWOB+cwTGCTcOliJUY44hfe3YtD8pP";
+  keys = [ pc-key test-vm-key test-vm-key-admin nas-admin-key nas-host-key ];
 in
 {
-  "vpn.env.age".publicKeys = keys;
+  "vpn.age".publicKeys = keys;
   "cloudflare-token.age".publicKeys = keys;
   "nextcloud.age".publicKeys = keys;
 }

f68e5709d2ca87dce344b6e5203021931fa81ca5
Felix Plamper <felix.plamper@tuta.io>
2026-07-12 17:06:59 +0200

    Fix postgres superuser

--- hosts/nix-nas/services/postgres.nix
+++ hosts/nix-nas/services/postgres.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 {
-
   # Setup Postgres
   services.postgresql = {
     enable = true;
-
-    # Copied from wiki to fix postgres login
+    # Copied from wiki to fix postgres login, extended so the postgres
+    # superuser can connect to any database (not just one matching its name)
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method optional_ident_map
-      local sameuser  all     peer        map=superuser_map
+      #type database  DBuser    auth-method optional_ident_map
+      local all        postgres peer        map=superuser_map
+      local sameuser   all      peer        map=superuser_map
     '';
     identMap = ''
       # ArbitraryMapName systemUser DBUser
